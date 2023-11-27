97153e1f10ac4e28e2c0d8f9aa9ac2ecbd697634
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-11-27 16:05:00 -0300

    refactor to typescript

---
+++ src/components/Comment.tsx
@@ -0,0 +1,56 @@
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+import { useState } from 'react';
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+        
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1);
+    }
+
+    return (
+        <div className={styles.comment}>
+            
+            <Avatar hasBorder={false} src="https://github.com/guirodrigues9876.png" alt=""/>
+        
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Guilherme Santos</strong>
+                            <time title="15 de Setembro às 17:15hrs" dateTime="2023-09-15 17:15:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} weight="regular" />
+                        </button>
+                    </header>
+                        
+                        <p>{content}</p>
+
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp size={24} weight="light"/>
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+
+            </div>
+        </div>
+    )
+}
